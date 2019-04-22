734ae3a23d273256e9ba0499a0a1b43a431e83a6
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-04-22 17:21:49 +0200

    Add js paints results from fetch

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputEl = document.querySelector('#TVshow');
+const buttonEl = document.querySelector('.button');
+const ulfavEl = document.querySelector('.favs');
+const ulresultsEl = document.querySelector('.results');
+
+function btnClickHandler(){
+  const searchText = inputEl.value;
+  fetch (`http://api.tvmaze.com/search/shows?q=${searchText}`)
+    .then (response => response.json())
+    .then (function (data) {
+      for (const object of data) {
+        const {show} = object;
+        const {name, image} = show;
+        const newShow = document.createElement('div');
+        newShow.classList.add('show');
+        const newP = document.createElement('p');
+        const pContent = document.createTextNode(name);
+        newP.appendChild(pContent);
+        const newImg = document.createElement('img');
+        if (image){
+          const {medium: imageMedium} = image;
+          newImg.src= imageMedium;
+        }else{
+          newImg.src= 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+        }
+        newShow.appendChild(newImg);
+        newShow.appendChild(newP);
+        ulresultsEl.appendChild(newShow);
+      }
+    });
+}
+
+buttonEl.addEventListener('click', btnClickHandler);
